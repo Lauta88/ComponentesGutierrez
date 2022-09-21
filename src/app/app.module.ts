cdd2744a40d1bba6d10f0943b2c8aeb2334a41c8
Lautaro Gutierrez <lauchagutierrez117@gmail.com>
2022-09-21 14:40:37 -0300

    segunda version desafio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './componentes/header/header.component';
+import { SideBarComponent } from './componentes/side-bar/side-bar.component';
+import { ContenidoComponent } from './componentes/contenido/contenido.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SideBarComponent,
+    ContenidoComponent
   ],
   imports: [
     BrowserModule,
